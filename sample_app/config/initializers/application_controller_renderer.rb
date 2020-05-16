572c1dcd811f31248e26d81739ab6e14d57fa14d
odakurasatoshi <xiaotiancang@sina.com>
2020-05-16 20:18:10 +0900

    [Finish]3-11

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0248e243b61f6265fc81d76df6816569f75eb96bdc935b46efc6866f910a99f0adaa8a0a5480e4c27a4b34461a7303851a7721b9d8ea7b84bf120b1dc52e4460'
